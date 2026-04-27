358994251bec6d5fafbfe8a6af61991e23a782b2
Mateo Maldini <mateomaldini.dev@gmail.com>
2026-04-27 19:04:55 -0300

    init project

---
+++ src/main/java/com/utn/corralon/CorralonApplication.java
@@ -0,0 +1,13 @@
+package com.utn.corralon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorralonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorralonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/corralon/CorralonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.corralon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorralonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
